fc512a5329905e1a3e17b2cc41c85feae328347d
peter <peter@soez.io>
2018-05-31 15:00:13 +0800

    实现日志功能

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ var flash = require('connect-flash');
 var config = require('config-lite')(__dirname);
 var routes = require('./routes');
 var pkg = require('./package');
+var winston = require('winston');
+var expressWinston = require('express-winston');
 
 var app = express();
 
@@ -52,8 +54,32 @@ app.use(function (req, res, next) {
     next();
 });
 
+// 正常请求的日志
+app.use(expressWinston.logger({
+    transports: [
+        new(winston.transports.Console)({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/success.log'
+        })
+    ]
+}));
 // 路由
 routes(app);
+// 错误请求的日志
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'logs/error.log'
+        })
+    ]
+}));
 
 app.use(function (err, req, res, next) {
     res.render('error', {
